7e8a78460a90c95c63f9e4f02c527f20cde96cf8
Marc Matias <mrcallenmatias@gmail.com>
2022-04-03 22:16:37 -0700

    final assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,4 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,38 +13,106 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("sentenceNameCapitalize", function() {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", function() {
 
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+    expect(sentenceNameCapitalize(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+// make a function that takes in an object
+// make a loop, then turn the name into another array
+// use chartAt and uppercase to capitalize the name
+// store the new array into a new variable
+// use map method to create a string of name and occupation using the old array and by calling the occupation from the old object
+
+function sentenceNameCapitalize(array){
+  completedArr = []
+  for (let i = 0; i < array.length; i++) {
+    names = array[i].name
+    sp = names.split(" ")
+    arr = []
+    for (let j = 0; j < sp.length; j++) {
+        arr.push(sp[j].charAt(0).toUpperCase() + sp[j].substring(1))
+    }
+    completedArr.push(arr.join(" "))
+  }
+  return array.map((value, key) => {
+    return `${completedArr[key]} is ${value.occupation}.`
+  })
+
+}
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+describe("remainderArray", function() {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", function() {
+
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+    expect(remainderArray(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remainderArray(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+// make a function that takes in an array
+// get the number data from array by using the filter method
+// use the map method or a loop to return an array of only the REMAINDERS of the numbers when divided by 3
+
+function remainderArray(array) {
+  filterArr =  array.filter(a => {
+      return typeof a === "number"
+  })
+  return filterArr.map(a => (a % 3))
+}
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("cubeArray", function() {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.", function() {
 
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+
+    expect(cubeArray(cubeAndSum1)).toEqual(99)
+    expect(cubeArray(cubeAndSum2)).toEqual(1125)
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+// create a function that takes in an array
+// make a new variable to store total
+// use map method to add the cube of a number to the new variable.
+// return the sum of all the numbers cubed.
+
+function cubeArray(array) {
+  let sum = 0
+  array.map(a => {
+      sum +=a**3
+      return sum
+  })
+  return sum
+}
